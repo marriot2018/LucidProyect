9a12c950d26896aac618f907a9717642e99ae466
marriot2018 <marriot_x@hotmail.com>
2025-06-25 09:40:48 -0600

    batteryqml

--- battery/main.cpp
+++ battery/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("battery", "Main");
+    engine.loadFromModule("battery", "BatteryWidget");
 
     return app.exec();
 }
